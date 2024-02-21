e95551324a43429a9e28247c2c78daef74237218
Praveen Mishra <nice.prav@gmail.com>
2024-02-22 01:04:59 +0530

    HashMap

---
+++ src/main/java/lp/neetcode/l1_arrayshashing/hashing/HashMapWorking.java
@@ -0,0 +1,12 @@
+package lp.neetcode.l1_arrayshashing.hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapWorking {
+  public static void main(String[] args){
+    Map<Integer, String> map = new HashMap<>();
+    map.put(1,"1");
+    System.out.println(map);
+  }
+}
